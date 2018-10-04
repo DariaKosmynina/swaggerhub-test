b3a85de114bad8707ea75ad73aa6d283b6924550
Daria.Kosmynina <daria.kosmynina@smartbear.com>
2018-10-04 08:44:50 +0000

    Generated code by SwaggerHub

---
+++ jaxrs/src/main/java/io/swagger/api/factories/StoreApiServiceFactory.java
@@ -0,0 +1,13 @@
+package io.swagger.api.factories;
+
+import io.swagger.api.StoreApiService;
+import io.swagger.api.impl.StoreApiServiceImpl;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2018-10-04T08:44:50.619Z")
+public class StoreApiServiceFactory {
+    private final static StoreApiService service = new StoreApiServiceImpl();
+
+    public static StoreApiService getStoreApi() {
+        return service;
+    }
+}
---
+++ jaxrs/src/main/java/io/swagger/api/factories/UserApiServiceFactory.java
@@ -0,0 +1,13 @@
+package io.swagger.api.factories;
+
+import io.swagger.api.UserApiService;
+import io.swagger.api.impl.UserApiServiceImpl;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2018-10-04T08:44:50.619Z")
+public class UserApiServiceFactory {
+    private final static UserApiService service = new UserApiServiceImpl();
+
+    public static UserApiService getUserApi() {
+        return service;
+    }
+}
